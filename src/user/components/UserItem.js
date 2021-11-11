3cacdb2ca92c3803c5d5417bc6c47b8b392e5e75
Moklocke <38056226+Moklocke@users.noreply.github.com>
2021-11-11 14:51:23 -0700

    working up through 48
    this is working.

--- src/user/components/UserItem.js
+++ src/user/components/UserItem.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Avatar from "../../shared/components/UIELements/Avatar";
-import Card from "../../shared/components/UIELements/Card";
+import Avatar from "../../shared/components/UIElements/Avatar";
+import Card from "../../shared/components/UIElements/Card";
 import './UserItem.css';
 
 const UsersItem = props => {
     return (
-        <li classname="user-item">
+        <li className="user-item">
             <Card className="user-item__content">
                 <Link to={`/${props.id}/places`}>
                 <div className="user-item__image">
